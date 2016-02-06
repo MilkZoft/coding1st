1d4582585c6ae7d26972ae995b3ed825643fdf53
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-02-05 16:17:44 -0800

    Getting project's path on docker-run.sh

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-read -p "Project's path: " projectPath
 read -p "Host Port: " hostPort
 read -p "Docker Image Name: " imageName
+
+projectPath="$(dirname "$(pwd)")"
 
 docker run -v $projectPath:/src -d -p $hostPort:3000 $imageName
