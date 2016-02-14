91b4baf0e85d51daec39c8640e7734d2da8e2573
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-02-14 00:39:18 -0800

    Fixing router.js

--- src/router.js
+++ src/router.js
@@ -2,24 +2,27 @@
 
 var config = require('./lib/config');
 var availableLanguages = config().languages.list.join('|');
-var defaultController;
-var homeController;
 
 module.exports = function(app) {
     // Loading controllers
-    defaultController = require('./controllers/' + config().controllers.default);
-    homeController = require('./controllers/home');
+    var defaultController = require('./controllers/' + config().controllers.default);
+    var homeController = require('./controllers/home');
+    var dashboardController = require('./controllers/dashboard');
+    var authController = require('./controllers/auth')
 
     // Loading necessary helpers
     var i18n = require('./lib/helpers/i18n');
     var utils = require('./lib/helpers/utils');
 
+  // Loading isMobile, basePath, currentLanguage and __
     app.use(function(req, res, next) {
+        res.locals.isConnected = true;
         res.locals.isMobile = utils.isMobile(req.headers['user-agent']);
         res.locals.config.basePath = config().baseUrl + i18n.getLanguagePath(req.url);
         res.locals.currentLanguage = i18n.getCurrentLanguage(req.url);
         res.__ = res.locals.__ = i18n.load(i18n.getCurrentLanguage(req.url));
         res.locals.basePath = res.locals.config.basePath;
+        res.locals.securityToken = res.session('securityToken');
 
         next();
     });
@@ -37,8 +40,10 @@ module.exports = function(app) {
 
     // Controllers dispatch
     app.use('/', defaultController);
+    app.use('/auth', authController);
     app.use('/:language(' + availableLanguages + ')', defaultController);
     app.use('/:language(' + availableLanguages + ')/home', homeController);
+    app.use('/:language(' + availableLanguages + ')/dashboard', dashboardController);
 
     // catch 404 and forward to error handler
     app.use(function(req, res, next) {
