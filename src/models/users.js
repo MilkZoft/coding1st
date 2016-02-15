bc8a60561fdafdeb1304f96d5f7aec09c59e1795
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-02-14 23:03:28 -0800

    Adding users model

---
+++ src/models/users.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var Model = require('../lib/helpers/model');
+var Users = new Model();
+var fields1 = ['network', 'networkId', 'username', 'password'];
+var fields2 = ['email', 'avatar', 'subscribed'];
+
+module.exports = {
+    getPrivilege: getPrivilege,
+    getUser: getUser,
+    save: save
+};
+
+function getPrivilege(user, callback) {
+    var procedure = Users.getProcedure('getPrivilege', user, fields1);
+
+    Users.query(procedure, callback, function(result, callback) {
+        var data = (result[0].length > 0) ? result[0] : false;
+
+        callback(data);
+    });
+}
+
+function getUser(user, callback) {
+    var procedure = Users.getProcedure('getUser', user, fields1);
+
+    Users.query(procedure, callback, function(result, callback) {
+        var data = (result[0].length > 0) ? result[0] : false;
+
+        callback(data);
+    });
+}
+
+function save(user, callback) {
+    var procedure = Users.getProcedure('saveUser', user, fields1.concat(fields2), {
+        password: 'encrypt'
+    });
+
+    Users.query(procedure, callback, function(result, callback) {
+        callback(result);
+    });
+}
