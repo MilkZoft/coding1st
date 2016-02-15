3819b2cc4774ae6d588f94153eaa869160c3c029
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-02-14 22:49:21 -0800

    Fixing auth controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -6,37 +6,35 @@ var config  = require('../lib/config');
 var twitter = require('../lib/helpers/twitter');
 
 router.get('/twitter', function(req, res) {
-  twitter.getOAuthRequestToken(function(tokens) {
-    var oauthSession = {
-      'token': tokens[0],
-      'tokenSecret': tokens[1]
-    };
+    twitter.getOAuthRequestToken(function(tokens) {
+        var oauthSession = {
+            'token': tokens[0],
+            'tokenSecret': tokens[1]
+        };
 
-    console.log('ENTRAAAA', oauthSession);
-
-    res.session('oauth', oauthSession);
-    res.redirect(twitter.getAuthenticateUrl(tokens[0]));
-  });
+        res.session('oauth', oauthSession);
+        res.redirect(twitter.getAuthenticateUrl(tokens[0]));
+    });
 });
 
 router.get('/twitter/callback', function(req, res) {
-  var oauthData = res.session('oauth');
+    var oauthData = res.session('oauth');
 
-  if (oauthData) {
-    var oauthVerifier = req.query['oauth_verifier'];
+    if (oauthData) {
+        var oauthVerifier = req.query['oauth_verifier'];
 
-    twitter.getOAuthAccessToken(
-      oauthData.token,
-      oauthData.tokenSecret,
-      oauthVerifier,
-      function(sessions) {
-        res.session('oauth', sessions[0]);
-        res.session('user', sessions[1]);
+        twitter.getOAuthAccessToken(
+            oauthData.token,
+            oauthData.tokenSecret,
+            oauthVerifier,
+            function(sessions) {
+                res.session('oauth', sessions[0]);
+                res.session('user', sessions[1]);
 
-        res.redirect(res.locals.basePath + '/users/validation');
-      }
-    );
-  }
+                res.redirect(res.locals.basePath + '/users/validation');
+            }
+        );
+    }
 });
 
 module.exports = router;
