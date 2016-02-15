f3c9d640a7020e3ecaf7a63a637b4dda789f82ad
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-02-14 23:00:15 -0800

    Adding blog controller

---
+++ src/controllers/blog.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    var visits = res.session('visits') || 0;
+
+    res.session('visits', ++visits);
+
+    res.render('blog/welcome', {
+        siteName: 'Coding1st',
+        visits: visits,
+        username: res.session('user').username
+    });
+});
+
+module.exports = router;
