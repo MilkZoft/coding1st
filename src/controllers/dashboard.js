d2bc7d43ed38f7a9d23548515098b27e95aeb2c4
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-02-14 22:50:22 -0800

    Adding dashboard

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,12 +1,124 @@
 var express = require('express');
 var router = express.Router();
+var user = require('../lib/helpers/user');
 
 router.get('/', function(req, res, next) {
-    var visits = res.session('visits') || 0;
+    res.profileAllowed(function(userInfo) {
+        if (userInfo) {
+            res.render('dashboard/index', {
+                userInfo: userInfo,
+                section: 'Dashboard',
+                layout: 'dashboard.hbs'
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+});
 
-    res.render('dashboard/index', {
-        section: 'Dashboard',
-        layout: 'dashboard.hbs'
+router.get('/ads/:action*?', function(req, res, next) {
+    var section = res.__.dashboard.modules.ads.name;
+
+    res.profileAllowed(function(userInfo) {
+        if (userInfo) {
+            if (req.params.action === 'add') {
+                section = res.__.dashboard.modules.ads.action;
+            }
+
+            res.render('dashboard/index', {
+                userInfo: userInfo,
+                section: section,
+                layout: 'dashboard.hbs'
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+router.get('/blog/:action*?', function(req, res, next) {
+    var section = res.__.dashboard.modules.blog.name;
+
+    res.profileAllowed(function(userInfo) {
+        if (userInfo) {
+            res.render('dashboard/index', {
+                userInfo: userInfo,
+                section: 'Blog',
+                layout: 'dashboard.hbs'
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+router.get('/config', function(req, res, next) {
+    res.profileAllowed(function(userInfo) {
+        if (userInfo) {
+            res.render('dashboard/index', {
+                userInfo: userInfo,
+                section: 'Config',
+                layout: 'dashboard.hbs'
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+router.get('/feedback', function(req, res, next) {
+    res.profileAllowed(function(userInfo) {
+        if (userInfo) {
+            res.render('dashboard/index', {
+                userInfo: userInfo,
+                section: 'Feedback',
+                layout: 'dashboard.hbs'
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+router.get('/pages/:action*?', function(req, res, next) {
+    res.profileAllowed(function(userInfo) {
+        if (userInfo) {
+            res.render('dashboard/index', {
+                userInfo: userInfo,
+                section: 'Pages',
+                layout: 'dashboard.hbs'
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+router.get('/polls/:action*?', function(req, res, next) {
+    res.profileAllowed(function(userInfo) {
+        if (userInfo) {
+            res.render('dashboard/index', {
+                userInfo: userInfo,
+                section: 'Polls',
+                layout: 'dashboard.hbs'
+            });
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+router.get('/users/:action*?', function(req, res, next) {
+    res.profileAllowed(function(userInfo) {
+        if (userInfo) {
+            res.render('dashboard/index', {
+                userInfo: userInfo,
+                section: 'Users',
+                layout: 'dashboard.hbs'
+            });
+        } else {
+            res.redirect('/');
+        }
     });
 });
 
