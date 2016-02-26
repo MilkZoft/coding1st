44313c4b6853f8a294b538383541d27064379f71
Carlos Santana <czantany@MacBook-Pro-de-Carlos.local>
2016-02-26 00:00:01 -0800

    Adding gulp-json-format to vendo task

--- gulp/vendo.js
+++ gulp/vendo.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var remoteSrc = require('gulp-remote-src');
+var jsonFormat = require('gulp-json-format');
 
 module.exports = function() {
     gulp.task('vendo', function() {
         remoteSrc(['en.json', 'es.json'], {
             base: 'http://local.coding1st.com/vendomatic/'
         })
+        .pipe(jsonFormat(4))
         .pipe(gulp.dest('./src/content/i18n/'));
     });
 };
